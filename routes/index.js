aacfd6a90c8c7926ea7451fac923e5fb8007919f
Khôi <63962075+ukiaqua@users.noreply.github.com>
2022-03-19 14:16:15 +0700

    Third Commit
    - New Register Page
    - Access by localhost:3000/signup

--- routes/index.js
+++ routes/index.js
@@ -10,5 +10,9 @@ router.get('/login', function(req, res, next) {
   res.render('login');
 });
 
+router.get('/signup', function(req, res, next) {
+  res.render('signup');
+});
+
 
 module.exports = router;
